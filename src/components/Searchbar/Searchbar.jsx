1b08e080c049540c1d391dc642d1ff13a2cea253
Valerii Droniak <droniak.valerii@gmail.com>
2023-01-01 02:19:06 +0200

    fixed searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,50 +1,61 @@
 import PropTypes from 'prop-types';
+import { Component } from 'react';
 import { Box } from 'components/Box/Box';
 import { Form, Input, SearchButton } from './Searchbar.styled';
 import { GoSearch } from 'react-icons/go';
 
-export const Searchbar = ({ onSubmit }) => {
-  const handleSubmit = evt => {
-    evt.preventDefault();
-    onSubmit(evt.currentTarget.lastChild.value.trim());
+export class Searchbar extends Component {
+  state = {
+    query: '',
   };
 
-  return (
-    <Box
-      as="header"
-      position="sticky"
-      zIndex="1100"
-      top={0}
-      left={0}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="64px"
-      pr="24px"
-      pl="24px"
-      pt="12px"
-      pb="12px"
-      color="white"
-      bg="primary"
-      boxShadow="go_it"
-    >
-      <Form onSubmit={handleSubmit}>
-        <SearchButton type="submit">
-          <GoSearch size="80%" />
-          {/* <span>Search</span> */}
-        </SearchButton>
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
 
-        <Input
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-        />
-      </Form>
-    </Box>
-  );
-};
+  handleSubmit = evt => {
+    evt.preventDefault();
+    this.props.onSubmit(this.state.query);
+  };
 
-Searchbar.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
+  handleChange = evt => {
+    this.setState({ query: evt.target.value.trim() });
+  };
+
+  render() {
+    return (
+      <Box
+        as="header"
+        position="sticky"
+        zIndex="1100"
+        top={0}
+        left={0}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="64px"
+        pr="24px"
+        pl="24px"
+        pt="12px"
+        pb="12px"
+        color="white"
+        bg="primary"
+        boxShadow="go_it"
+      >
+        <Form onSubmit={this.handleSubmit}>
+          <SearchButton type="submit">
+            <GoSearch size="80%" />
+          </SearchButton>
+
+          <Input
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleChange}
+          />
+        </Form>
+      </Box>
+    );
+  }
+}
